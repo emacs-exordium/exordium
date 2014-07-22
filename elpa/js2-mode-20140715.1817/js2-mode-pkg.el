d2a40f42f8dbd5f25283879537b6cd871cd2308e
Philippe Grenet <phil.grenet@gmail.com>
2014-07-21 20:36:13 -0400

    Added js2-mode

---
+++ elpa/js2-mode-20140715.1817/js2-mode-pkg.el
@@ -0,0 +1,4 @@
+(define-package "js2-mode" "20140715.1817" "Improved JavaScript editing mode"
+  '((emacs "24.1"))
+  :url "https://github.com/mooz/js2-mode/" :keywords
+  '("languages" "javascript"))
