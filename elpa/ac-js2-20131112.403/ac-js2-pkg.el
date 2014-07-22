c61fc09af6912c417164e5efa994c5322c58830e
Philippe Grenet <phil.grenet@gmail.com>
2014-07-22 07:12:04 -0400

    Added ac-js2 (autocomplete for js2)

---
+++ elpa/ac-js2-20131112.403/ac-js2-pkg.el
@@ -0,0 +1,4 @@
+(define-package "ac-js2" "20131112.403" "Auto-complete source for Js2-mode, with navigation"
+  '((js2-mode "20090723")
+    (skewer-mode "1.4"))
+  :url "https://github.com/ScottyB/ac-js2")
---
+++ elpa/simple-httpd-20140613.955/simple-httpd-pkg.el
@@ -0,0 +1,1 @@
+(define-package "simple-httpd" "20140613.955" "pure elisp HTTP server" (quote ((cl-lib "0.3"))))
