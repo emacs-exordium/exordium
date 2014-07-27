639da59b60ec7836881d8565639cefdcd6e37fb6
Philippe Grenet <phil.grenet@gmail.com>
2014-07-27 19:14:22 -0400

    Added ido-find-recentf (is this really useful though?)

--- init_extensions.el
+++ init_extensions.el
@@ -20,7 +20,9 @@
 (require 'ido)
 (ido-mode 'both)
 
-;; Display ido results vertically, rather than horizontally
+;; Display ido results vertically, rather than horizontally (see also
+;; ido-vertical-mode):
+;;
 ;; (setq ido-decorations
 ;;       '("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]"
 ;;         " [Not readable]" " [Too big]" " [Confirm]"))
@@ -31,6 +33,22 @@
 ;;   (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
 ;;   (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
 ;; (add-hook 'ido-setup-hook 'ido-define-keys)
+
+;; Open recent files with IDO.
+;; `abbreviate-file-name' abbreviates home dir to ~/ in the file list
+;; Custom abbreviations can be added to `directory-abbrev-alist'.
+(require 'recentf)
+
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+
+(defun ido-find-recentf ()
+  "Use ido to select a recently opened file from the `recentf-list'"
+  (interactive)
+  (find-file
+   (ido-completing-read "Recentf open: "
+                        (mapcar 'abbreviate-file-name recentf-list)
+                        nil t)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; CUA
