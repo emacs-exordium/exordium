99656198c54c9e9426d52d03404035f5af807ef0
Philippe Grenet <phil.grenet@gmail.com>
2014-08-19 18:39:54 -0400

    Minor changes in helm

--- modules/init-helm.el
+++ modules/init-helm.el
@@ -8,7 +8,14 @@
 
 (require 'helm)
 
+;;; Do not show these files in helm buffer
+(add-to-list helm-boring-file-regexp-list
+      '("\\.tsk$" "\\.log$"))
+
+;; Use `recentf-list' instead of `file-name-history' in `helm-find-files'.
+;;(setq helm-ff-file-name-history-use-recentf t)
+
 (global-set-key (kbd "C-c h") 'helm-mini)
-(global-set-key (kbd "M-h") 'helm-M-x)
+(global-set-key (kbd "C-c m") 'helm-M-x)
 
 (provide 'init-helm)
