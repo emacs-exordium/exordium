929521a33af8ee0ab77954e055e970837490261d
Philippe Grenet <phil.grenet@gmail.com>
2014-09-27 09:53:30 -0400

    IDO ignored files and buffers patterns

--- modules/init-ido.el
+++ modules/init-ido.el
@@ -3,20 +3,24 @@
 (require 'ido)
 (ido-mode 'both)
 
-;; Display ido results vertically, rather than horizontally (see also
-;; ido-vertical-mode):
-;;
-;; (setq ido-decorations
-;;       '("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]"
-;;         " [Not readable]" " [Too big]" " [Confirm]"))
-;; (defun ido-disable-line-truncation ()
-;;   (set (make-local-variable 'truncate-lines) nil))
-;; (add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
-;; (defun ido-define-keys () ;; C-n/p is more intuitive in vertical layout
-;;   (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
-;;   (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
-;; (add-hook 'ido-setup-hook 'ido-define-keys)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Ignored files and buffers
 
+(setq ido-ignore-files '("\\`#"
+                         "\\`.#"
+                         "\\`\\.\\./"
+                         "\\`\\./"
+                         "\\`00"
+                         "\\`.*\\.tsk"
+                         "\\`ported\\..*"))
+
+(setq ido-ignore-buffers '("\\` "
+                           "\\*Buffer List\\*"
+                           "\\*Help\\*"
+                           "\\*Messages\\*"
+                           "\\*Completions\\*"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Open recent files with IDO.
 ;; `abbreviate-file-name' abbreviates home dir to ~/ in the file list
 ;; Custom abbreviations can be added to `directory-abbrev-alist'.
