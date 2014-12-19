29487aee0320185c9eb62f940ec8bc8ac443c491
Julien Bibollet <julien.bibollet@gmail.com>
2014-12-18 23:11:22 -0500

    Adding ruby module, setting up enh-ruby-mode

---
+++ modules/init-ruby.el
@@ -0,0 +1,8 @@
+;;;; Configuration for Ruby
+;;;; https://github.com/zenspider/enhanced-ruby-mode
+
+(autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
+
+(provide 'init-ruby)
