f82580fb507fa631a33bc447c38f4c020ca178f3
Philippe Grenet <pgrenet@bloomberg.com>
2015-01-10 11:13:10 -0500

    Magit: C-c g opens magit full screen, and restores windows after

--- modules/init-git.el
+++ modules/init-git.el
@@ -5,6 +5,8 @@
 ;;; ----------------- ---------------------------------------------------------
 ;;; C-c g             Magit status
 
+(require 'magit)
+
 ;;; Ctrl-c g = magit-status
 (define-key global-map [(control c)(g)] 'magit-status)
 
@@ -13,5 +15,20 @@
   (require 'git-gutter-fringe)
   (global-git-gutter-mode t)
   (diminish 'git-gutter-mode))
+
+;;; Make `magit-status' run alone in the frame, and then restore the old window
+;;; configuration when you quit out of magit.
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
 
 (provide 'init-git)
