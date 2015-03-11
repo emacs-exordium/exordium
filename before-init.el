30b3313df75ecb78a5d4c707ed452d43c0f54700
Fred Cassirer <fcassirer@bloomberg.net>
2015-03-11 14:56:44 -0400

    Added proxy settings for dev network

--- before-init.el
+++ before-init.el
@@ -1,5 +1,19 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Proxies for packages
+;;; See DRQS 54913427
+
+;;
+;; Bloomberg specific proxy setup, need to comment this out for non BB environment
+
+(if (string= (getenv "ORGANIZATION") 'bloomberg)
+(setq url-proxy-services
+      '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+        ("http" . "devproxy.bloomberg.com:82")
+        ("https" . "devproxy.bloomberg.com:82")))
+)
+
+  ; Windows on Bloomberg if true
 (if (string= (getenv "BLOOMBERG_NETWORK") 'corporate)
-  ; something for Windows on Bloomberg if true
     (setq url-proxy-services
       '(("no_proxy" . "^\\(localhost\\|10.*\\)")
         ("http" . "proxy-lon.bloomberg.com:80")
