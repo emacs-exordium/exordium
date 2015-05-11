8e7478d1f3c39cc54957645b1689ad48031c9daf
Philippe Grenet <pgrenet@bloomberg.com>
2015-05-11 17:10:31 -0400

    Added loud face for TODOs in markdown documents

--- modules/init-markdown.el
+++ modules/init-markdown.el
@@ -5,4 +5,13 @@
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 
+;;; Loud face for TODOs in markdown documents
+(require 'init-prefs)
+(require 'markdown-mode)
+(when exordium-font-lock
+  (setq markdown-mode-font-lock-keywords-core
+        (list
+         (cons markdown-regex-italic '(2 markdown-italic-face))
+         (cons "\\<\\(TODO\\|FIXME\\|TBD\\):" '(1 font-lock-warning-face)))))
+
 (provide 'init-markdown)
