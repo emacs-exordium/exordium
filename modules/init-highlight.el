d5d460b69b739ca136b4f42606cab730635f4b60
Philippe Grenet <phil.grenet@gmail.com>
2016-07-30 17:03:39 -0400

    Add coloring tool to help maintain themes
    This is a simple tool to help maintain theme files: it displays the
    color codes and names in their actual color in the current buffer.
    
    Usage example:
    
    - Open a theme file such as color-theme-material.el.
    - Run M-x highlight-colors-from-list. It prompts for a sexp.
    - Enter something that evaluates to an a-list associating color
      symbols to hex codes, like
        ((color1 . "#123456") (color2 . "#789012") ...)
    
    For color-theme-material.el, the expression is simply: material-colors
    
    For color-theme-tomorrow, it would be something like:
    (cdr (assoc 'night tomorrow-colors))

--- modules/init-highlight.el
+++ modules/init-highlight.el
@@ -31,6 +31,7 @@
 
 ;;; Highlight symbol under point automatically after a small delay.
 
+(eval-when-compile (require 'highlight-symbol))
 (when exordium-highlight-symbol
   (require 'highlight-symbol)
   (highlight-symbol-nav-mode)
@@ -68,5 +69,77 @@ highlighted using different colors at one time."
            (hi-lock-face-symbol-at-point)))))
 
 (global-set-key (kbd "C-c C-SPC") #'exordium-highlight-symbol)
+
+
+;;; Highlight color name and Hex values in buffer.
+;;;
+;;; Mostly convenient for editing Emacs theme files:
+;;; run M-x highlight-colors-from-alist and enter an expression like
+;;; material-colors or (cdr (assoc 'night tomorrow-colors)).
+;;; This will highlight the hex codes and the colors names in buffer.
+;;; Note that the expression must evaluate to an a-list like:
+;;; ((color1 . "#123456") (color2 . "#345678"))
+;;;
+;;; Otherwise, there is also the rainbow-mode in Elpa which is more generic but
+;;; cannot be set to specific colors.
+
+;; Hexadecimal colors
+(defconst exordium-hex-colors-font-lock-keywords
+  '(("[^&]\\(#\\(?:[0-9a-fA-F]\\{3\\}\\)+\\{1,4\\}\\)"
+     (1 (exordium-colorize-itself 1))))
+  "Font-lock keywords to add for hexadecimal colors.")
+
+(defun highlight-colors-from-list (arg)
+  "Highlight any hex color code and any symbol associated with a
+color code in ARG, in the current buffer. ARG is an expression
+evaluating to an a-list like: ((symbol . hex-string) (symbol . hex-string))"
+  (interactive "XExpression: ")
+  ;; Highlight hex color codes:
+  (font-lock-add-keywords nil exordium-hex-colors-font-lock-keywords t)
+  ;; Highlight color symbols defined in arg:
+  (let ((arg-font-lock-keywords
+         `((,(regexp-opt (mapcar (lambda (pair)
+                                   (concat "," (if (stringp (car pair))
+                                                   (car pair)
+                                                 (symbol-name (car pair)))))
+                                 arg))
+            (0 (exordium-colorize-by-assoc ',arg))))))
+    (font-lock-add-keywords nil `(,@arg-font-lock-keywords) t))
+  ;; Force refresh
+  (font-lock-mode 1))
+
+(defun exordium-colorize-by-assoc (alist)
+  "Colorize a match with its association from ASSOC-LIST."
+  (let ((color (substring (match-string-no-properties 0) 1)))
+    (exordium-colorize-match (cdr (assoc-string color alist t)))))
+
+(defun exordium-colorize-match (color &optional match)
+  "Return a matched string propertized with a face whose
+background is COLOR. The foreground is computed according to the
+luminance and is either white or black."
+  (let ((match (or match 0)))
+    (put-text-property
+     (match-beginning match) (match-end match)
+     'face `((:foreground ,(if (> 0.5 (exordium-x-color-luminance color))
+                               "white" "black"))
+             (:background ,color)))))
+
+(defun exordium-colorize-itself (&optional match)
+  "Colorize a match with itself."
+  (exordium-colorize-match (match-string-no-properties (or match 0)) match))
+
+(defun exordium-color-luminance (red green blue)
+  "Calculate the luminance of color composed of RED, BLUE and GREEN.
+Return a value between 0 and 1."
+  (/ (+ (* .2126 red) (* .7152 green) (* .0722 blue)) 256))
+
+(defun exordium-x-color-luminance (color)
+  "Calculate the luminance of a color string (e.g. \"#ffaa00\", \"blue\").
+Return a value between 0 and 1."
+  (let* ((values (x-color-values color))
+         (r      (/ (car values) 256.0))
+         (g      (/ (cadr values) 256.0))
+         (b      (/ (caddr values) 256.0)))
+    (exordium-color-luminance r g b)))
 
 (provide 'init-highlight)
