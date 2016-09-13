0a0416b0c6846ac77304cb8cda58e6c848c86754
Abhijit <dev.abhijitb@gmail.com>
2016-09-13 19:07:03 +0530

    ycm config generator function

--- local/my_pref.el
+++ local/my_pref.el
@@ -9,4 +9,8 @@
 (global-set-key (kbd "C-x r x") 'helm-M-x)        ;; bookmarks - set and jump
 (global-set-key (kbd "M-y") 'helm-show-kill-ring) ;; rotate previously copies
 
+
+;; bring ycm config genrator function
+(require 'ycm_generator)
+
 (provide 'my_pref)
