87e4c7bfbbff9bf1286e07aa96ce47a97495ce35
Matthew Persico <MPERSICO5@bloomberg.net>
2017-04-03 15:04:46 -0400

    Account for name switch on rtags ac and helm modules

--- modules/init-rtags.el
+++ modules/init-rtags.el
@@ -126,7 +126,7 @@
 (require 'init-lib)
 (require 'init-prefs)
 (require 'rtags)
-(require 'rtags-ac)
+(require 'ac-rtags)
 (require 'auto-complete-c-headers)
 (require 'projectile)
 
@@ -412,7 +412,7 @@ just returns the path and content of the header file which
 
 ;;; AC source for RTags
 
-(defun rtags-ac-init ()
+(defun ac-rtags-init ()
   (unless rtags-diagnostics-process
     (rtags-diagnostics)))
 
@@ -433,7 +433,7 @@ Note that RTags becomes the only source for auto-complete in all
 C and C++ buffers. Also note that RTags Diagostics must be turned
 on."
   (interactive)
-  (require 'rtags-ac)
+  (require 'ac-rtags)
   (setq rtags-completions-enabled t)
   (add-hook 'c++-mode-hook
             (lambda ()
