8e557fac3e3bdc54f7984fd51781909e96b3c46b
Przemyslaw Kryger <pkryger@gmail.com>
2017-10-14 13:13:43 +0100

    Add move region up/down
    I've been finding this really useful when using Eclipse.

--- modules/init-look-and-feel.el
+++ modules/init-look-and-feel.el
@@ -20,6 +20,9 @@
 ;;; C-=               Expand region by semantic units
 ;;; M-C-=             Contract region by semantic units
 ;;;
+;;; M-<up>            Move selected region up
+;;; M-<down>          Move selected region down
+;;;
 ;;; F10               Speedbar
 ;;; ----------------- ---------------------------------------------------------
 
@@ -208,6 +211,30 @@
 (require 'expand-region)
 (global-set-key (kbd "C-=") (function er/expand-region))
 (global-set-key (kbd "M-C-=") (function er/contract-region))
+
+;;; Move regions up and down (from https://www.emacswiki.org/emacs/MoveRegion)
+(defun move-region (start end n)
+  "Move the current region up or down by N lines."
+  (interactive "r\np")
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (let ((start (point)))
+      (insert line-text)
+      (setq deactivate-mark nil)
+      (set-mark start))))
+
+(defun move-region-up (start end n)
+  "Move the current line up by N lines."
+  (interactive "r\np")
+  (move-region start end (if (null n) -1 (- n))))
+
+(defun move-region-down (start end n)
+  "Move the current line down by N lines."
+  (interactive "r\np")
+  (move-region start end (if (null n) 1 n)))
+
+(global-set-key (kbd "M-<up>") 'move-region-up)
+(global-set-key (kbd "M-<down>") 'move-region-down)
 
 
 ;;; File saving and opening
