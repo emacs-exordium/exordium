3f879b106e1d78482ac98d8e0db939172b57271f
Przemek Kryger <pkryger@bloomberg.net>
2022-02-01 18:32:21 +0000

    Remove reminder CI scripts missing functions W/As

--- .ci/compilation.sh
+++ .ci/compilation.sh
@@ -12,10 +12,5 @@ ${EMACS} -Q --batch \
 (progn
    (setq debug-on-error t
          user-emacs-directory "'${EMACS_DIR}'")
-   (when (and (version< emacs-version "27")
-              (not (fboundp (quote define-fringe-bitmap))))
-     (defun define-fringe-bitmap (&rest args)
-       "Workaround for missing function in pre-27 non GUI purcell/setup-emacs."
-       (car args)))
    (load-file "'${EMACS_DIR}'/init.el"))' \
          -f batch-byte-compile ${EMACS_DIR}/{modules,extensions}/*.el
