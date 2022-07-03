bdfb5faba9fdd2bd753bbf299255e62565daca5a
Przemysław Kryger <pkryger@gmail.com>
2022-07-03 23:36:01 +0100

    Update CI for emacs 28 (#198)
    * Ignore more elpa packages
    
    * Add emacs 28.1 to ci and fix pkryger-taps tests
    
    * Fix exordium-flip-string-quotes
    
    * CI fix: sleep then grab a lock on conflict

--- .ci/first-start.sh
+++ .ci/first-start.sh
@@ -6,9 +6,15 @@ set -e
 EMACS_DIR="$(cd ${GITHUB_WORKSPACE:-~}/${1:-.emacs.d}; pwd -P)/"
 EMACS="${EMACS:=emacs}"
 
+# Redefine ask-user-about-lock as the melpa seems to stumble on it
+# quite often in macos runs. Strategy: wait for 5s then grab the lock
+# anyway.
 ${EMACS} -Q --batch \
          --eval '
 (progn
    (setq debug-on-error t
          user-emacs-directory "'${EMACS_DIR}'")
+   (defun ask-user-about-lock (file opponent)
+     (sleep-for 5)
+     t)
    (load-file "'${EMACS_DIR}'/init.el"))'
