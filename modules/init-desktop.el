1e9abdb22eeadf395e5b54e3f61ff68ac009444e
Steve Downey <sdowney2@bloomberg.net>
2023-01-09 16:38:45 -0500

    Reload the desktop when emacs server loads first frame
    Allow restore of desktop frame configuration as well as files being reloaded
    when running in daemon mode.

--- modules/init-desktop.el
+++ modules/init-desktop.el
@@ -1,6 +1,17 @@
 ;;;; Configuration of desktop state and history
 
-(desktop-save-mode 1) ;;the state of Emacs is saved from one session to another.
+(when (not (daemonp))
+  (desktop-save-mode 1)) ;;the state of Emacs is saved from one session to another.
+
+(defun exordium-restore-desktop ()
+  (setq desktop-path '("~/.emacs.d/"))
+  (setq desktop-save t)
+  (message (format "loading desktop from %s" desktop-path))
+  (desktop-read)
+  (desktop-save-mode 1))
+
+(when (daemonp)
+  (add-hook 'server-after-make-frame-hook #'exordium-restore-desktop))
 
 (savehist-mode t) ;; minibuffer history is saved
 
