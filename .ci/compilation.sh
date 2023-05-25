ec30296de6ec6e03e0112293438adaea8d2e7116
Przemek Kryger <pkryger@gmail.com>
2023-05-25 10:53:51 +0100

    [CI] Quotting as suggested by shellcheck

--- .ci/compilation.sh
+++ .ci/compilation.sh
@@ -3,14 +3,14 @@
 set -x
 set -e
 
-EMACS_DIR="$(cd ${GITHUB_WORKSPACE:-~}/${1:-.emacs.d}; pwd -P)/"
-EMACS="${EMACS:=emacs}"
+EMACS_DIR="$(cd "${GITHUB_WORKSPACE:-~}"/"${1:-.emacs.d}"; pwd -P)/"
+EMACS=${EMACS:=emacs}
 
 # Byte compile all `.el` files in modules, themes, and extensions
 ${EMACS} -Q --batch \
          --eval '
 (progn
    (setq debug-on-error t
-         user-emacs-directory "'${EMACS_DIR}'")
-   (load-file "'${EMACS_DIR}'/init.el"))' \
-         -f batch-byte-compile ${EMACS_DIR}/{modules,extensions}/*.el
+         user-emacs-directory "'"${EMACS_DIR}"'")
+   (load-file "'"${EMACS_DIR}"'/init.el"))' \
+         -f batch-byte-compile "${EMACS_DIR}"/{modules,extensions}/*.el
--- .ci/unit-test-cl.sh
+++ .ci/unit-test-cl.sh
@@ -2,14 +2,14 @@
 
 set -x
 set -e
-EMACS_DIR="$(cd ${GITHUB_WORKSPACE:-~}/${1:-.emacs.d}; pwd -P)/"
-EMACS="${EMACS:=emacs}"
+EMACS_DIR="$(cd "${GITHUB_WORKSPACE:-~}"/"${1:-.emacs.d}"; pwd -P)/"
+EMACS=${EMACS:=emacs}
 
 ${EMACS} -Q --batch \
          --eval '
 (progn
-   (setq user-emacs-directory "'${EMACS_DIR}'")
-   (load-file "'${EMACS_DIR}'/init.el")
+   (setq user-emacs-directory "'"${EMACS_DIR}"'")
+   (load-file "'"${EMACS_DIR}"'/init.el")
    (setq debug-on-error t)
-   (load-file "'${EMACS_DIR}'/modules/init-util-cl.t.el")
+   (load-file "'"${EMACS_DIR}"'/modules/init-util-cl.t.el")
    (ert-run-tests-batch-and-exit))'
