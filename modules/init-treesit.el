cade18503fe02b303699a024596969553aea47d8
Steve Downey <sdowney2@bloomberg.net>
2023-07-31 22:18:25 -0400

    Use add-hook for adding and forwarding hooks
    Reenable init-lsp
    Add lsp-mode hooks to c++- and c-ts-modes
    Use add-hook in the forwarding hook in case the underlying mode has not been
    loaded yet.

--- modules/init-treesit.el
+++ modules/init-treesit.el
@@ -2,16 +2,17 @@
 
 ;;;; Configuration for Treesitter
 
+(defun exordium--add-forward-ts-hook (mode)
+  (when-let ((ts-hook (intern (concat (symbol-name mode) "-ts-mode-hook")))
+             (hook (intern (concat (symbol-name mode) "-mode-hook")))
+             (_ (and (symbolp ts-hook) (symbolp hook))))
+    (add-hook ts-hook
+                 #'(lambda ()
+                     (run-hooks hook)))))
+
 (unless (getenv "ci_tests")
 (if (and (version< "29" emacs-version) (treesit-available-p))
     (progn
-      (defun exordium--add-forward-ts-hook (mode)
-        (when-let ((ts-hook (intern (concat (symbol-name mode) "-ts-mode-hook")))
-                   (hook (intern (concat (symbol-name mode) "-mode-hook")))
-                   (_ (and (symbolp ts-hook) (symbolp hook))))
-          (add-to-list ts-hook
-                       #'(lambda ()
-                           (run-hooks hook)))))
 
       (use-package treesit-auto
         :requires treesit
@@ -25,7 +26,7 @@
         :ensure nil
         :after lsp-mode
         :config
-        (mapc exordium--add-forward-ts-hook
+        (mapc #'exordium--add-forward-ts-hook
               '(
                 LaTeX
                 c
@@ -40,22 +41,23 @@
                 ))
         (setq treesit-font-lock-level 4)))
 
-  (use-package tree-sitter-langs)
-  (use-package tree-sitter
-    :diminish
-    :after (tree-sitter-langs)
-    :hook
-    (tree-sitter-after-on . tree-sitter-hl-mode)
-    :custom
-    (font-lock-maximum-decoration t)
-    :config
-    (when-let ((language-name (alist-get 'ruby-mode
-                                         tree-sitter-major-mode-language-alist)))
+  (progn
+    (use-package tree-sitter-langs)
+    (use-package tree-sitter
+      :diminish
+      :after (tree-sitter-langs)
+      :hook
+      (tree-sitter-after-on . tree-sitter-hl-mode)
+      :custom
+      (font-lock-maximum-decoration t)
+      :config
+      (when-let ((language-name (alist-get 'ruby-mode
+                                           tree-sitter-major-mode-language-alist)))
+        (add-to-list 'tree-sitter-major-mode-language-alist
+                     (cons 'enh-ruby-mode language-name)))
       (add-to-list 'tree-sitter-major-mode-language-alist
-                   (cons 'enh-ruby-mode language-name)))
-    (add-to-list 'tree-sitter-major-mode-language-alist
-                 (cons 'forge-post-mode 'markdown))
-    (global-tree-sitter-mode)))
+                   (cons 'forge-post-mode 'markdown))
+      (global-tree-sitter-mode))))
 ) ;; (unless (getenv "ci_tests")
 
 (provide 'init-treesit)
