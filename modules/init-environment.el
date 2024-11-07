51d3973340b745faa530dc453350186e0c66e8b1
Przemysław Kryger <pkryger@gmail.com>
2024-11-07 13:18:38 +0000

    Cleanup init-environment

--- modules/init-environment.el
+++ modules/init-environment.el
@@ -1,23 +1,29 @@
-;;;; Environment variables
+;;; init-environment.el --- Environment variables ;; -*- lexical-binding: t; -*-
+;;; Commentary:
+;;
+
+;;; Code:
 
 (defconst exordium-osx (eq system-type 'darwin)
-  "Non-nil if we are on a Mac")
+  "Non-nil if we are on a Mac.")
 
 (defconst exordium-linux (string-match "linux" (prin1-to-string system-type))
-  "Non-nil if we are on Linux")
+  "Non-nil if we are on Linux.")
 
 (defconst exordium-nw (not window-system)
-  "Non-nil if emacs is started in -nw mode")
+  "Non-nil if Emacs is started in -nw mode.")
 
 (defconst exordium-xwindow (eq window-system 'x)
-  "Non-nil if we are in X-Window")
+  "Non-nil if we are in X-Window.")
 
 (defconst exordium-bloomberg (and exordium-xwindow
                                   (getenv "MBIG_NUMBER"))
-  "Non-nil if we are at Bloomberg")
+  "Non-nil if we are at Bloomberg.")
 
 (defconst exordium-current-user
   (getenv
    (if (equal system-type 'windows-nt) "USERNAME" "USER")))
 
 (provide 'init-environment)
+
+;;; init-environment.el ends here
