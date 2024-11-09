33052573dea4e2df3d554ad23ab70e2fea70d2eb
Przemysław Kryger <pkryger@gmail.com>
2024-11-09 16:30:20 +0000

    Cleanup init-linum

--- modules/init-linum.el
+++ modules/init-linum.el
@@ -1,19 +1,29 @@
-;;;; Line numbers
-;;;
-;;; The number one thing that can make Emacs slow is the line number mode.
-;;; This module turn it on for all modes except the ones that for which we
-;;; don't need it (e.g. using an exception list).
-;;;
-;;; See also init-ui.el
+;;; init-linum.el --- Line numbers                   -*- lexical-binding: t -*-
 
-(require 'init-prefs)
-(use-package display-line-numbers :ensure nil)
+;;; Commentary:
+;;
+;; The number one thing that can make Emacs slow is the line number mode.
+;; This module turn it on for all modes except the ones that for which we
+;; don't need it (e.g. using an exception list).
+;;
+;; See also init-ui.el
+
+;;; Code:
+
+(eval-when-compile
+  (unless (featurep 'init-require)
+    (load (file-name-concat (locate-user-emacs-file "modules") "init-require"))))
+(exordium-require 'init-prefs)
+
+(require 'display-line-numbers)
 
 (defun exordium--inhibit-line-numbers-p ()
+  "Return non nil if line numbers should be inhibited in current buffer.
+Otherwise return nil."
   (or (minibufferp)
       (and exordium-inhibit-line-numbers-modes
-           (cl-find-if #'(lambda (mode)
-                           (derived-mode-p mode))
+           (cl-find-if (lambda (mode)
+                         (derived-mode-p mode))
                        exordium-inhibit-line-numbers-modes))
       (and exordium-inhibit-line-numbers-star-buffers
            (eq 0 (string-match "*" (buffer-name))))
@@ -24,9 +34,12 @@
 (define-globalized-minor-mode exordium-global-display-line-numbers-mode
   display-line-numbers-mode
   (lambda () (unless (exordium--inhibit-line-numbers-p)
-               (display-line-numbers-mode))))
+               (display-line-numbers-mode)))
+  :group 'exordium)
 
 (when exordium-display-line-numbers
   (exordium-global-display-line-numbers-mode t))
 
 (provide 'init-linum)
+
+;;; init-linum.el ends here
