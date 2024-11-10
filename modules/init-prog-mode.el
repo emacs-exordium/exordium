5ab0b113e50206c887891b9faf07b28ad570e407
Przemysław Kryger <pkryger@gmail.com>
2024-11-10 12:33:09 +0000

    Cleanup init-prog-mode

--- modules/init-prog-mode.el
+++ modules/init-prog-mode.el
@@ -1,9 +1,21 @@
-;; Shared prog-mode configuration
+;;; init-prog-mode.el --- Shared prog-mode configuration -*- lexical-binding: t -*-
 
-(require 'init-prefs)
+;;; Commentary:
+;;
+
+;;; Code:
+(eval-when-compile
+  (unless (featurep 'init-require)
+    (load (file-name-concat (locate-user-emacs-file "modules") "init-require"))))
+(exordium-require 'init-prefs)
+
+(require 'newcomment)
+
+(use-package cmake-mode
+  :defer t)
 
 (define-minor-mode exordium-show-trailing-whitespace-mode
-  "Enables `show-trailing-whitespace'."
+  "Enable `show-trailing-whitespace'."
   :init-value nil
   :lighter nil
   (progn (setq show-trailing-whitespace exordium-show-trailing-whitespace-mode)))
@@ -14,23 +26,25 @@
   :lighter nil
   (progn (setq require-final-newline exordium-require-final-newline-mode)))
 
-(add-hook 'prog-mode-hook 'show-paren-mode)
-(add-hook 'prog-mode-hook 'exordium-show-trailing-whitespace-mode)
-(add-hook 'prog-mode-hook 'exordium-require-final-newline-mode)
+(add-hook 'prog-mode-hook #'show-paren-mode)
+(add-hook 'prog-mode-hook #'exordium-show-trailing-whitespace-mode)
+(add-hook 'prog-mode-hook #'exordium-require-final-newline-mode)
 
 (use-package flyspell
+  :ensure nil
   :if (eq exordium-spell-check :prog)
   :diminish 'flyspell-mode
-  :init
-  (add-hook 'prog-mode-hook 'flyspell-prog-mode))
-
+  :hook
+  (prog-mode . flyspell-prog-mode)
+  :bind
+  (:map flyspell-mode-map
+        ("C-." . nil))) ;; unbind as it colides with company, see init-company.el
 
 ;;; Electric pair: automatically close parenthesis, curly brace etc.
 ;;; `electric-pair-open-newline-between-pairs'.
 (setq electric-pair-open-newline-between-pairs t)
 (when exordium-enable-electric-pair-mode
-  (add-hook 'prog-mode-hook 'electric-pair-mode))
-
+  (add-hook 'prog-mode-hook #'electric-pair-mode))
 
 ;;; The return key
 (cond (exordium-enable-newline-and-indent
@@ -41,30 +55,27 @@
 
 
 ;;; Fill comments, comment regions
-(require 'newcomment)
 (setq comment-auto-fill-only-comments 1)
 (define-key prog-mode-map (kbd "\C-c\C-c") (function comment-region))
-
 
 ;;; Step through compile errors
 (global-set-key (quote [f10]) (quote next-error))
 (global-set-key (quote [(control f10)]) (quote previous-error))
 
-
 
 ;;; Font lock changes
 
-;;; Display TODO: and FIXME: and TBD: in warning face
-(when exordium-font-lock
-  (defun add-keywords-for-todos ()
+(defun exordium--add-keywords-for-todos ()
+  "Display TODO: and FIXME: and TBD: in warning face."
     (font-lock-add-keywords
      nil
      '(("\\<\\(FIXME\\):" 1 font-lock-warning-face prepend)
        ("\\<\\(TBD\\):" 1 font-lock-warning-face prepend)
        ("\\<\\(TODO\\):" 1 font-lock-warning-face prepend))))
-  (add-hook 'prog-mode-hook 'add-keywords-for-todos))
 
-
+(when exordium-font-lock
+  (add-hook 'prog-mode-hook #'exordium--add-keywords-for-todos))
 
 (provide 'init-prog-mode)
-;; End of file
+
+;;; init-prog-mode.el ends here
