c5954fbc2a1e8714a1e35378961d9a23cbd56a53
Przemysław Kryger <pkryger@gmail.com>
2024-11-13 10:42:14 +0000

    Cleanup init-rtags-helm

--- modules/init-rtags-helm.el
+++ modules/init-rtags-helm.el
@@ -1,64 +1,49 @@
-;;;; RTags and Helm integration
-;;;
-;;; -------------- -------------------------------------------------------
-;;; Key            Definition
-;;; -------------- -------------------------------------------------------
-;;; M-C-g          `rtags-helm-select-taglist' = select a symbol in the
-;;;                current file using Helm.
-;;; C-c r r        `helm-flycheck' show rtags & flycheck errors in helm buffer
-;;;                when buffer is in `c-mode'
-;;; C-x c f        `helm-flycheck' show flycheck errors in helm buffer
-;;; -------------- -------------------------------------------------------
+;;; init-rtags-helm.el --- RTags and Helm integration -*- lexical-binding: t -*-
 
-(use-package rtags)
-(use-package helm)
-(use-package helm-rtags)
-(use-package flycheck)
-(use-package cc-mode :ensure nil)
-(require 'init-prefs)
+;;; Commentary:
+;;
+;;
+;; -------------- -------------------------------------------------------
+;; Key            Definition
+;; -------------- -------------------------------------------------------
+;; M-C-g          `rtags-helm-select-taglist' = select a symbol in the
+;;                current file using Helm.
+;; -------------- -------------------------------------------------------
 
-(defun exordium-helm-flycheck ()
-  "Ensure `flycheck-mode' is enabled and run `helm-flycheck'."
-  (interactive)
-  (unless flycheck-mode
-    (flycheck-mode)
-    (diminish 'flycheck-mode))
-  (helm-flycheck))
-
-(use-package helm-flycheck
-  :bind
-  (:map c-mode-base-map
-        ("C-c r r" . #'exordium-helm-flycheck)
-   :map helm-command-map
-   ("f" . #'exordium-helm-flycheck)))
-
+;;; Code:
+(eval-when-compile
+  (unless (featurep 'init-require)
+    (load (file-name-concat (locate-user-emacs-file "modules") "init-require"))))
+(exordium-require 'init-prefs)
+(exordium-require 'init-rtags)
+(exordium-require 'init-helm)
 
 (defcustom rtags-helm-show-variables nil
-  "Whether `rtags-helm-select-taglist' shows variables and parameters"
+  "Whether `rtags-helm-select-taglist' shows variables and parameters."
   :group 'rtags
   :type 'boolean)
 
 (defcustom rtags-helm-show-enums nil
-  "Whether `rtags-helm-select-taglist' shows enums"
+  "Whether `rtags-helm-select-taglist' shows enums."
   :group 'rtags
   :type 'boolean)
 
 (when (or exordium-helm-everywhere exordium-rtags-helm-everywhere)
   (setq rtags-helm-show-variables t)
-  (setq rtags-helm-show-enums t)
-  (setq rtags-use-helm t))
+  (setq rtags-helm-show-enums t))
 
 (defun rtags-helm-sort-list (pairs)
-  (sort pairs #'(lambda (p1 p2) (< (cdr p1) (cdr p2)))))
+  "Sort list of PAIRS using their cdrs."
+  (sort pairs (lambda (p1 p2) (< (cdr p1) (cdr p2)))))
 
 (defun rtags-helm-jump-to-line (line)
-  ;;Compiler-happy equivalent of (goto-line line):
+  "Compiler-happy equivalent of (goto-line LINE)."
   (goto-char (point-min))
   (forward-line (1- line))
   (recenter))
 
 (defun rtags-helm-propertize-function (text)
-  "Return a colored string for a method, constructor or function declaration"
+  "Return propertized TEXT for a method, constructor or function declaration."
   (cond ((string-match "^\\(.*\\) \\(.*\\)::\\(.*\\)$" text)
          (let ((return-type (match-string-no-properties 1 text))
                (class (match-string-no-properties 2 text))
@@ -82,7 +67,7 @@
         (t text)))
 
 (defun rtags-helm-propertize-variable (text)
-  "Return a colored string for a variable declaration"
+  "Return propertized TEXT for a variable declaration."
   (cond ((string-match "^const \\(.*\\) \\(.*\\)$" text)
          (let ((type (match-string-no-properties 1 text))
                (var (match-string-no-properties 2 text)))
@@ -99,7 +84,7 @@
         (t text)))
 
 (defun rtags-helm-propertize-macro (text)
-  "Return a colored string for a #include or a #define"
+  "Return propertized TEXT for a #include or a #define."
   (cond ((string-match "^#include \\(.*\\)$" text)
          (let ((file (match-string-no-properties 1 text)))
            (format "%s %s"
@@ -109,8 +94,8 @@
 
 ;;;###autoload
 (defun rtags-helm-select-taglist ()
-  "Display the list of symbols of the current file in an Helm
-buffer (classes, functions, variables, enums and other)"
+  "Display the list of symbols of the current file in an Helm buffer.
+For example: classes, functions, variables, enums and other."
   (interactive)
   (let* ((fn (buffer-file-name))
          functions classes variables enums macros other)
@@ -120,49 +105,70 @@ buffer (classes, functions, variables, enums and other)"
       ;;(message "%s" (buffer-string))
       (unless (= (point-min) (point-max))
         (while (not (eobp))
-          (let ((line (buffer-substring-no-properties (point-at-bol) (point-at-eol))))
+          (let ((line (buffer-substring-no-properties (line-beginning-position)
+                                                      (line-end-position))))
             (when (string-match "^\\(.*:\\)\\([0-9]+\\)\\(:[0-9]+:\\)\t\\(.*\\)\t\\(.*\\)$" line)
-                (let ((loc-start (match-string-no-properties 1 line))
-                      (linenum (match-string-no-properties 2 line))
-                      (loc-end (match-string-no-properties 3 line))
+                (let ((linenum (match-string-no-properties 2 line))
                       (text (match-string-no-properties 4 line))
                       (type (match-string-no-properties 5 line)))
                   (cond ((or (string= type "FunctionDecl")
                              (string= type "CXXMethod")
                              (string= type "CXXConstructor")
                              (string= type "CXXDestructor"))
-                         (add-to-list 'functions
-                                      (cons (rtags-helm-propertize-function text)
-                                            (string-to-number linenum))))
+
+                         (cl-pushnew (cons (rtags-helm-propertize-function text)
+                                           (string-to-number linenum))
+                                     functions
+                                     :test (lambda (e1 e2)
+                                             (and (equal (car e1) (car e2))
+                                                  (eql (cdr e1) (cdr e2))))))
                         ((or (string= type "ClassDecl")
                              (string= type "StructDecl"))
-                         (add-to-list 'classes
-                                      (cons (propertize text 'face 'font-lock-type-face)
-                                            (string-to-number linenum))))
+                         (cl-pushnew (cons (propertize text 'face 'font-lock-type-face)
+                                           (string-to-number linenum))
+                                     classes
+                                     :test (lambda (e1 e2)
+                                             (and (equal (car e1) (car e2))
+                                                  (eql (cdr e1) (cdr e2))))))
                         ((string= type "FieldDecl")
-                         (add-to-list 'variables
-                                      (cons (rtags-helm-propertize-variable text)
-                                            (string-to-number linenum))))
+                         (cl-pushnew (cons (rtags-helm-propertize-variable text)
+                                           (string-to-number linenum))
+                                     variables
+                                     :test (lambda (e1 e2)
+                                             (and (equal (car e1) (car e2))
+                                                  (eql (cdr e1) (cdr e2))))))
                         ((and rtags-helm-show-variables
                               (or (string= type "VarDecl")
                                   (string= type "ParmDecl")))
-                         (add-to-list 'variables
-                                      (cons (rtags-helm-propertize-variable text)
-                                            (string-to-number linenum))))
+                         (cl-pushnew (cons (rtags-helm-propertize-variable text)
+                                           (string-to-number linenum))
+                                     variables
+                                     :test (lambda (e1 e2)
+                                             (and (equal (car e1) (car e2))
+                                                  (eql (cdr e1) (cdr e2))))))
                         ((and rtags-helm-show-enums
                               (or (string= type "EnumDecl")
                                   (string= type "EnumConstantDecl")))
-                         (add-to-list 'enums
-                                      (cons text (string-to-number linenum))))
+                         (cl-pushnew (cons text (string-to-number linenum))
+                                     enums
+                                     :test (lambda (e1 e2)
+                                             (and (equal (car e1) (car e2))
+                                                  (eql (cdr e1) (cdr e2))))))
                         ((or (string= type "macro definition")
                              (string= type "include directive")
                              (string= type "inclusion directive"))
-                         (add-to-list 'macros
-                                      (cons (rtags-helm-propertize-macro text)
-                                            (string-to-number linenum))))
+                         (cl-pushnew (cons (rtags-helm-propertize-macro text)
+                                           (string-to-number linenum))
+                                     macros
+                                     :test (lambda (e1 e2)
+                                             (and (equal (car e1) (car e2))
+                                                  (eql (cdr e1) (cdr e2))))))
                         (t
-                         (add-to-list 'other
-                                      (cons text (string-to-number linenum))))))))
+                         (cl-pushnew (cons text (string-to-number linenum))
+                                     other
+                                     :test (lambda (e1 e2)
+                                             (and (equal (car e1) (car e2))
+                                                  (eql (cdr e1) (cdr e2))))))))))
           (forward-line))))
     ;; Display them in Helm
     (helm :sources
@@ -184,4 +190,7 @@ buffer (classes, functions, variables, enums and other)"
 
 (define-key c-mode-base-map [(meta control g)] 'rtags-helm-select-taglist)
 
+
 (provide 'init-rtags-helm)
+
+;;; init-rtags-helm.el ends here
