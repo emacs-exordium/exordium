8d77c592335a840b669aecf05e839f18239c8688
Przemysław Kryger <pkryger@gmail.com>
2024-11-17 13:48:10 +0000

    [CI] Add tests for exordium-require

---
+++ modules/init-require.t-dummy.el
@@ -0,0 +1,10 @@
+;;; init-require.t-dummy.el --- Dummy feature for init-require.t -*- lexical-binding: t -*-
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+(provide 'init-require.t-dummy)
+
+;;; init-require.t-dummy.el ends here
