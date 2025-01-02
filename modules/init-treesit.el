0e4967cda6b8884fe772c0d8358b307c2488095e
Przemysław Kryger <pkryger@gmail.com>
2025-01-02 12:31:23 +0000

    feat(git,treesit,company)!: Add support for Conventional Commits
    Add `git-commit-ts-mode` that uses Connventional Commits grammar for
    highlighting in git commits.
    
    Also add a small `company` backend to support (at least some of) Conventional
    Commits keywords (types and footers).
    
    For Conventional Commits specification see  https://www.conventionalcommits.org.
    
    BREAKING CHANGE: commit buffers now use `git-commit-ts-mode`

--- modules/init-treesit.el
+++ modules/init-treesit.el
@@ -8,6 +8,7 @@
   (unless (featurep 'init-require)
     (load (file-name-concat (locate-user-emacs-file "modules") "init-require"))))
 (exordium-require 'init-prefs)
+(exordium-require 'init-git)
 
 (defun exordium--add-forward-ts-hook (mode)
   "Add hook to a ts-MODE that will run hooks from MODE."
@@ -33,10 +34,43 @@
 (if (and (version< "29" emacs-version) (treesit-available-p))
     (progn
       (message "Enabling built-in treesit and external treesit-auto")
+      (use-package git-commit-ts-mode
+        :functions (exordium--git-commit-ts-verify)
+        :init
+        (use-package git-commit
+          :ensure magit
+          :defer t
+          :custom
+          (git-commit-major-mode #'git-commit-ts-mode))
+
+        (defun exordium--git-commit-ts-verify ()
+          "Used for `flyspell-generic-check-word-predicate' in `git-commit-ts-mode'."
+          ;; Like `flyspell-generic-progmode-verify', but use faces from
+          ;; `git-commit-ts-mode' and include nil (no face) for regular message
+          ;; body text.  Unfortunately, the `font-lock' in a `treesit' mode
+          ;; kicks in too late to fontify heading prefixes, and trailers, so
+          ;; these tend to be highlighted as well.
+          (unless (eql (point) (point-min))
+            (let ((f (get-text-property (1- (point)) 'face)))
+              (memq f '(nil
+                        git-commit-ts-comment-face
+                        git-commit-ts-title-face
+                        git-commit-ts-overflow-face
+                        git-commit-ts-breaking-change-value-face)))))
+        :hook (git-commit-ts-mode
+               . exordium--git-commit-ts-setup-hanging-trailers)
+        :config
+        (put 'git-commit-ts-mode
+             'flyspell-mode-predicate
+             #'exordium--git-commit-ts-verify))
+
       (use-package treesit-auto
         :after treesit
         :demand t
+        :autoload (make-treesit-auto-recipe)
         :commands (global-treesit-auto-mode)
+        :defines (treesit-auto-recipe-alist
+                  treesit-auto-mode-langs)
         :custom
         (treesit-auto-install (if (boundp 'treesit-auto-install)
                                   treesit-auto-install
@@ -44,6 +78,15 @@
                                   'prompt))
                               "Disable automatic grammar downloading in CI")
         :config
+        (unless (memq 'gitcommit treesit-auto-langs)
+          (push
+           (make-treesit-auto-recipe
+            :lang 'gitcommit
+            :ts-mode 'git-commit-ts-mode
+            :url "https://github.com/gbprod/tree-sitter-gitcommit"
+            :ext "\\COMMIT_EDITMSG\\'")
+           treesit-auto-recipe-list)
+          (push 'gitcommit treesit-auto-langs))
         (global-treesit-auto-mode))
 
       (use-package treesit
@@ -65,6 +108,7 @@
                 css
                 dockerfile
                 elixir
+                git-commit
                 go
                 go-mod
                 graphql
