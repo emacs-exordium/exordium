9cfc478a1217cc666480b12bbe34decfaf400914
Przemysław Kryger <pkryger@gmail.com>
2025-03-18 17:59:02 +0100

    refactor(company): Use external company-forge
    I developed package [company-forge] starting with `exordium-company-assignees`
    and extending its cababilities. The package, available on MELPA, provides
    completions not only for assignees (users) but also for teams and for issues
    and pull requests. It also provides icons for completions and uses
    `quickhelp-string` and `doc-buffer` to provide more information about issues
    and pull requests.
    
    [company-forge] https://github.com/pkryger/company-forge.el

--- modules/init-company.el
+++ modules/init-company.el
@@ -22,9 +22,7 @@
 (exordium-require 'init-git)
 
 (require 'cl-lib)
-
-(when (version< "29" emacs-version)
-  (exordium-require 'init-forge))
+(require 'compat)
 
 (use-package company
   :diminish "CA"
@@ -58,60 +56,8 @@
   :commands (company-begin-backend)
   :functions (exordium--company-conventional-commits-type-p)
   :init
-  (use-package forge-core
-    :ensure forge
-    :defer t
-    :autoload (forge-get-repository))
-
   (use-package git-commit
     :ensure magit)
-
-  (defun exordium-company-assignees (command &optional arg &rest _ignored)
-    "A `company-mode' backend for assigneees in `forge-mode' repository."
-    (interactive (list 'interactive))
-    (cl-case command
-      (interactive (company-begin-backend 'exordium-company-assignees))
-      (prefix
-       (save-match-data
-         (when (and (or (bound-and-true-p git-commit-mode)
-                        (derived-mode-p 'forge-post-mode
-                                        'git-commit-elisp-text-mode))
-                    (forge-get-repository 'full)
-                    (looking-back
-                     (rx "@"
-                         (zero-or-one
-                          (group alphanumeric
-                                 (repeat 0 38
-                                         (or alphanumeric
-                                             (seq (any "-/") alphanumeric))))))
-                     (max (- (point) 40)
-                          (line-beginning-position))))
-           ;; IDK how to match end of a 'symbol' that is equal to an "@" or is
-           ;; equal to "@foo" in neither `git-commit-mode' nor
-           ;; `forge-post-mode'. Hence it's handled manually.  The
-           ;; `looking-back' above matches an "@" or an "@foo". When it was the
-           ;; latter there was a match in group 1.  Now, check if this is at
-           ;; the very end of the "@" or "@foo".  Note that "@<point>@" also
-           ;; matches. Probably a few other characters, substituting the second
-           ;; "@" in latter pattern, would also give a positive result. Yet, in
-           ;; such a case the `match' is "", so that's all fine - all
-           ;; candidates will be shown.
-           (when (or (save-match-data (looking-at "\\W"))
-                     (= (point) (point-max)))
-             (cons (or (match-string 1) "") t)))))
-      (candidates (when-let* ((repo (forge-get-repository :tracked?))
-                              ((slot-exists-p repo 'assignees))
-                              ((slot-boundp repo 'assignees))
-                              (assignees (oref repo assignees)))
-                    (cl-remove-if-not
-                     (lambda (assignee)
-                       (string-prefix-p arg assignee))
-                     (mapcar (lambda (assignee)
-                               (propertize (cadr assignee)
-                                           'full-name (caddr assignee)))
-                             assignees))))
-      (annotation (when-let* ((assignee (get-text-property 0 'full-name arg)))
-                    (format " [%s]" assignee)))))
 
   (defconst exordium--company-conventional-commits-types
     (list
@@ -276,8 +222,17 @@ See https://www.conventionalcommits.org for details."
 
   :config
   ;; This is block is deferred , so this backed will end up first
-  (add-to-list 'company-backends 'exordium-company-assignees)
   (add-to-list 'company-backends 'exordium-company-conventional-commits))
+
+(when (version< "29" emacs-version) ;; Since Emacs-29
+ (use-package company-forge
+  :config
+  (company-forge-icons-mode) ;; Display icons
+  (advice-add #'forge--pull ;; Reset cache after forge pull
+              :filter-args #'company-forge-reset-cache-after-pull)
+  (add-to-list 'company-backends 'company-forge)
+  (add-hook 'completion-at-point-functions
+            #'company-forge-completion-at-point-function)))
 
 (use-package company-statistics
   :after (company)
